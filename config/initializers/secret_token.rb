f0466a1b66fa4beb17606dd181adc3ab22a04b87
Rik Albers <rik.albers@student.uva.nl>
2013-10-29 21:15:11 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TutApp2::Application.config.secret_key_base = '11c9d929fafb859269bb57c5888a1103b8405c1f1f6db0d7d3d813f4c1cd1cfeab913efc57f08f8d01624ae79dc12ec246a8cf081c9c592f54c32a0b81c3523c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TutApp2::Application.config.session_store :cookie_store, key: '_tut_app_2_session'
